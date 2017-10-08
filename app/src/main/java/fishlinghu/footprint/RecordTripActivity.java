499347e2367678af3772a920dbee62105932b766
fishlinghu <fishlinghu@gmail.com>
2017-10-08 03:34:08 -0400

    Add trip recording and trip completing pages

---
+++ app/src/main/java/fishlinghu/footprint/RecordTripActivity.java
@@ -0,0 +1,13 @@
+package fishlinghu.footprint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_trip);
+    }
+}
---
+++ app/src/main/java/fishlinghu/footprint/TripCompleteActivity.java
@@ -0,0 +1,13 @@
+package fishlinghu.footprint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripCompleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_complete);
+    }
+}
