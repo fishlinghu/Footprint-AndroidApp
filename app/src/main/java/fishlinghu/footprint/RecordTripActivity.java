33f33e1ed2f99f4c573f9f15ee80fbde808c41d1
fishlinghu <fishlinghu@gmail.com>
2017-10-15 19:13:52 -0400

    Initialize onClick function for buttons in recordTripActivity

--- app/src/main/java/fishlinghu/footprint/RecordTripActivity.java
+++ app/src/main/java/fishlinghu/footprint/RecordTripActivity.java
@@ -1,7 +1,10 @@
 package fishlinghu.footprint;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecordTripActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class RecordTripActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_trip);
+
+        Button button_check_in = findViewById(R.id.button_check_in);
+        button_check_in.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(RecordTripActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
+        Button button_finish_trip = findViewById(R.id.button_finish_trip);
+        button_finish_trip.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(RecordTripActivity.this, TripCompleteActivity.class));
+                finish();
+            }
+        });
     }
 }
