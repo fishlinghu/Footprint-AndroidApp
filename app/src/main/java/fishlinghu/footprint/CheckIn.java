81cc6a391c34f10ec2b4701b93d3223b87166f85
fishlinghu <fishlinghu@gmail.com>
2017-10-20 00:06:30 -0400

    Add date_time to checkIn class

--- app/src/main/java/fishlinghu/footprint/CheckIn.java
+++ app/src/main/java/fishlinghu/footprint/CheckIn.java
@@ -2,6 +2,8 @@ package fishlinghu.footprint;
 
 import android.location.Location;
 
+import java.util.Date;
+
 /**
  * Created by fishlinghu on 10/16/17.
  */
@@ -9,10 +11,12 @@ import android.location.Location;
 public class CheckIn {
     private Location location;
     private String photoUrl;
+    private Date date_time;
 
-    public CheckIn(Location location, String photo_url) {
+    public CheckIn(Location location, String photo_url, Date date_time) {
         this.location = location;
         this.photoUrl = photo_url;
+        this.date_time = date_time;
     }
 
     public Location getLocation() {
