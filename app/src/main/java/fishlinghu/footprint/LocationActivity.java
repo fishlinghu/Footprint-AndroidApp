07fec85dd1d15e854e25d0b6fdc3bf6df01d1229
fishlinghu <fishlinghu@gmail.com>
2017-11-13 01:42:37 -0500

    Finish photo downloading function

--- app/src/main/java/fishlinghu/footprint/LocationActivity.java
+++ app/src/main/java/fishlinghu/footprint/LocationActivity.java
@@ -1,10 +1,14 @@
 package fishlinghu.footprint;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -15,15 +19,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static fishlinghu.footprint.RecordTripActivity.getResizedBitmap;
 
 public class LocationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private CheckIn current_check_in;
+
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference storage_reference;
+
+    private FirebaseUser google_user;
+    private String account_email;
+
+    long SIXTEEN_MEGABYTE = 1024 * 1024 * 16;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,18 +78,83 @@ public class LocationActivity extends AppCompatActivity
 
         current_check_in = (CheckIn) getIntent().getSerializableExtra("current_check_in");
 
+        // set download button function
+        Button button_download = findViewById(R.id.button_download);
+        button_download.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LocationActivity.this,
+                        "The photo is still loading",
+                        Toast.LENGTH_LONG
+                ).show();
+            }
+        });
+
+        // set location name
         TextView textView_location_name = findViewById(R.id.textView_location_name);
         textView_location_name.setText(current_check_in.getLocationName());
         textView_location_name.setGravity(Gravity.CENTER_HORIZONTAL);
 
+        // set location description
         TextView textView_location_description = findViewById(R.id.textView_location_description);
         textView_location_description.setText(current_check_in.getDescription());
 
+        // set visiting time
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm");
         String visiting_time = sdf.format(current_check_in.getDateTime());
         TextView textView_visiting_time = findViewById(R.id.textView_visiting_time);
         textView_visiting_time.setText("Visited on " + visiting_time);
         textView_visiting_time.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        // make the filename for photo
+        SimpleDateFormat sdf_2 = new SimpleDateFormat("yyyyMMdd-HHmm");
+        final String filename = sdf_2.format(current_check_in.getDateTime()) + ".jpg";
+
+        // get current user and email, get photo from firebase
+        google_user = FirebaseAuth.getInstance().getCurrentUser();
+        account_email = google_user.getEmail();
+
+        storage_reference = storage.getReferenceFromUrl("gs://footprint-aff8d.appspot.com")
+                .child("images")
+                .child(account_email);
+
+        StorageReference photo_ref = storage_reference.child(
+                current_check_in.getPhotoUrl()
+        );
+        photo_ref.getBytes(SIXTEEN_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                final Bitmap temp_bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                ImageView temp_image_view = findViewById(R.id.imageView_location_photo);
+                temp_image_view.setImageBitmap(temp_bitmap);
+
+                // set button behavior as storing photo
+                Button button_download = findViewById(R.id.button_download);
+                button_download.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        storePhoto(temp_bitmap, filename, LocationActivity.this);
+                    }
+                });
+            }
+        });
+    }
+
+    public static void storePhoto(Bitmap bitmap, String filename, Context context) {
+        File mediaFile = new File(
+                Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + filename
+                );
+        try {
+            FileOutputStream fos = new FileOutputStream(mediaFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, fos);
+            fos.close();
+            Toast.makeText(context,
+                    "Photo stored at " + Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + filename,
+                    Toast.LENGTH_LONG
+            ).show();
+        } catch (Exception e) {
+            Log.d("DEBUG---", e.toString());
+        }
     }
 
     @Override
