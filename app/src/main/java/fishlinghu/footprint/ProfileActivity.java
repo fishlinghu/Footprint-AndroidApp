96135594cc7c9781717a68a29ba34bf8bb1eed02
fishlinghu <fishlinghu@gmail.com>
2017-11-28 23:06:36 -0500

    Fix bugs caused by no trip

--- app/src/main/java/fishlinghu/footprint/ProfileActivity.java
+++ app/src/main/java/fishlinghu/footprint/ProfileActivity.java
@@ -2,15 +2,12 @@ package fishlinghu.footprint;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -151,11 +148,13 @@ public class ProfileActivity extends AppCompatActivity
                                 .child("users")
                                 .child(account_email.replace(".", ","))
                                 .child("trips").getValue();
-                for (String key : trip_key.keySet()) {
-                    trip_list.add(
-                            dataSnapshot.child("trips").child(key).getValue(Trip.class)
-                    );
-                    trip_key_list.add(key);
+                if (trip_key != null) {
+                    for (String key : trip_key.keySet()) {
+                        trip_list.add(
+                                dataSnapshot.child("trips").child(key).getValue(Trip.class)
+                        );
+                        trip_key_list.add(key);
+                    }
                 }
 
                 // show the found trips on user's profile
